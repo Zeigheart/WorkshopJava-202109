191a119214aef58d783a6ab676c812def61b2470
Zeigheart <apiwat.kl@kbtg.tech>
2021-09-06 14:36:37 +0700

    Update application

--- demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
+++ demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -1,13 +1,20 @@
 package com.example.demoapp;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(DemoappApplication.class, args);
+		//Bean
+		System.out.println(context.getBeanDefinitionCount());
+		String[] beanNames=context.getBeanDefinitionNames();
+		Arrays.stream(beanNames).forEach(System.out::println);
 	}
 
 }
