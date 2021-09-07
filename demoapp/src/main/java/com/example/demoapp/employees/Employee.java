26951baf0a789ec819ed73326de89eee9102e825
Zeigheart <apiwat.kl@kbtg.tech>
2021-09-07 15:26:58 +0700

    Disable initial data in test

--- demoapp/src/main/java/com/example/demoapp/employees/Employee.java
+++ demoapp/src/main/java/com/example/demoapp/employees/Employee.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tbl_employee")
 public class Employee {
 		@Id
 		@GeneratedValue(strategy=GenerationType.AUTO)
--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -3,7 +3,6 @@ package com.example.demoapp.employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 	
 }
