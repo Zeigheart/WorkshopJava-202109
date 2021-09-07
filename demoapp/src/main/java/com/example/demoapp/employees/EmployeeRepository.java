6e1da0397bd10b5cd52d34991a46e36ccefba6c1
Zeigheart <apiwat.kl@kbtg.tech>
2021-09-07 10:17:56 +0700

    Workshop07 Repository Test

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.example.demoapp.employees;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+	
 }
